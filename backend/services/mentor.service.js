8870a116bd05cd60c9ab37e5d34c41ee5050fee3
Pranshu Shakya <pranshushakya2027@gmail.com>
2025-07-30 11:32:51 +0530

    refactor: streamline mentor retrieval by excluding sensitive fields

--- backend/services/mentor.service.js
+++ backend/services/mentor.service.js
@@ -2,16 +2,15 @@ import UserModel from "../models/user.model.js";
 import ServiceModel from "../models/service.model.js";
 
 export const getAllMentors = async () => {
-    return await UserModel.find({ role: "mentor" });
+    return await UserModel.find({ role: "mentor" }).select("-password -__v -refreshToken -otp -otpExpiry");
 };
 
 export const getMentorById = async (id) => {
-    return await UserModel.findOne({ _id: id, role: "mentor" });
+    return await UserModel.findOne({ _id: id, role: "mentor" }).select("-password -__v -refreshToken -otp -otpExpiry");
 };
 
 export const getMentorByFullname = async (fullname) => {
-    const mentor = await UserModel.findOne({ fullname, role: "mentor" });
+    const mentor = await UserModel.findOne({ fullname, role: "mentor" }).select("-password -__v -refreshToken -otp -otpExpiry");
     if (!mentor) return null;
-    console.log("Mentor found: ", mentor);
     return mentor;
 };
