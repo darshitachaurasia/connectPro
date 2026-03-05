1d1005b544ba76cf4ee1bf4cf6a9d6e8c10e00cc
Darshita Chaurasia <darshitachaurasia07@gmail.com>
2026-03-05 21:13:35 +0530

    resolved cors issue

--- backend/app.js
+++ backend/app.js
@@ -24,13 +24,22 @@ import asyncHandler from "./utils/asyncHandler.js";
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://connect-pro-rust.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+  })
 );
 app.use(express.json());
 app.use(cookieParser());
