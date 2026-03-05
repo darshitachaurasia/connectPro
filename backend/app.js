55cb40242eb37c70e6c54d71742211c1f8938544
Darshita Chaurasia <darshitachaurasia07@gmail.com>
2026-03-06 01:31:32 +0530

    added frontend url

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const allowedOrigins = [
+  "https://connect-pro-9z7w.vercel.app/ai",
   "http://localhost:5173",
   "https://connect-pro-rust.vercel.app",
 ];
